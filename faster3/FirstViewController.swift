defbad4a8c1adc64171cf4d0ce1b93b8aa1b28b2
Rafael Martins <rafael.mws@gmail.com>
2015-04-21 21:31:59 -0300

    Initial Commit

---
+++ faster3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  faster3
+//
+//  Created by Rafael Martins on 21/04/15.
+//  Copyright (c) 2015 Rafael Martins. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ faster3/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  faster3
+//
+//  Created by Rafael Martins on 21/04/15.
+//  Copyright (c) 2015 Rafael Martins. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
